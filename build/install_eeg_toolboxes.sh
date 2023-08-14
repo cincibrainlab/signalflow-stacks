ccf7b1ae46200f1ab58ce0042f3c3b7d09f3e70f
NSuer <Nathan.Suer@cchmc.org>
2023-08-14 13:22:23 -0400

    Update install_eeg_toolboxes.sh
    Updated to Include other dependencies for preprocess scripts

--- build/install_eeg_toolboxes.sh
+++ build/install_eeg_toolboxes.sh
@@ -1,10 +1,20 @@
-
 # Add installation for additional packages below. Root folder is /TOOLKITS
 
+# EEGLAB
 git clone --recurse-submodules https://github.com/sccn/eeglab.git
+# SignalFlow PreProcessing
 git clone --recurse-submodules https://github.com/cincibrainlab/SignalFlowEEG.git
+# Brainstorm
 git clone --recurse-submodules https://github.com/brainstorm-tools/brainstorm3.git
+# Happe
 git clone https://github.com/PINE-Lab/HAPPE.git
+# MADE
 git clone https://github.com/ChildDevLab/MADE-EEG-preprocessing-pipeline.git
+#BCT
+git clone https://github.com/brainlife/BCT.git
+#BRAPH
+git clone https://github.com/softmatterlab/BRAPH.git
+#SpectralEvents 
+git clone https://github.com/jonescompneurolab/SpectralEvents.git
 
 chown -R 1000:0 /TOOLBOXES
