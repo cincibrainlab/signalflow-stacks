d7e4ef66e5d4ee8aa3288c7a051c1f2ce945dfd6
NSuer <122294714+NSuer@users.noreply.github.com>
2023-09-01 10:17:39 -0400

    update

--- build/start_sfpreprocessing_example.sh
+++
@@ -1,8 +0,0 @@
-
-# Example docker run command to start workstation
-# Alternatively a docker-compose file can be used
-
-
-
-docker run --rm -it --shm-size=512m \
-    -p 6901:6901 -v //mnt/c/Users/ernie/Documents/:/srv -e VNC_PW=password -e START_XFCE4=1 --gpus all cincibrainlab/signalflow-development:focal
